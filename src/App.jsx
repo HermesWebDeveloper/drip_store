c6dc9fdb4dd166e469b34f720e298e546c7367b9
mrdanp07 <marcosdanielpontes07@gmail.com>
2024-07-20 14:38:39 -0300

    Adicionando alterações com react

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Nav from './components/Nav'
+import OfertaESpecial from './components/OfertaESpecial'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Nav />
+      <OfertaESpecial />
     </>
   )
 }
