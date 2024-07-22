733487270ea9b43a71739d57a6521130bd2bbea9
Janderson Almeida <janderson38@live.com>
2024-07-22 10:27:44 -0300

    deu certo o botao

--- src/components/NovasColecoes.jsx
+++ src/components/NovasColecoes.jsx
@@ -1,35 +1,56 @@
 import "./NovasColecoes.css";
-import nomevari1 from "../assets/Camisa Supreme.png"
-import nomevari2 from "../assets/Tenis Adids.png" 
-import nomevari3 from "../assets/Fone Bass.png" 
+import nomevari1 from "../assets/CamisaSupreme.png"
+import nomevari2 from "../assets/TenisAdidas.png"
+import nomevari3 from "../assets/FoneBass.png"
+<link rel="stylesheet" href="" />
+
+
 
 
 export default function NovasColecoes() {
 
     return (
-        <div><p>Novas Coleções</p>
+        <div>
+            <p className="id"><b>Coleções em destaque</b></p>
             <section className="colecoes-destaque">
-                <div className="product-card">
-                    <div className="supreme">
-                        <img src={nomevari1} alt=""></img>
-                            <h2>Novo drop Supreme</h2>
-                            <button>Comprar</button>
+                <div className="card-produto">
+                        <div className="fundo-bg">
+                        <div className="rotulo">
+                            <h2>Novo drop </h2>
+                            <h2>Supreme</h2>
+                            <button><b className="button-comprar">Comprar</b></button></div>
+                            <b className="button-desconto">30% OFF</b>
+                            <div className="imagem-produtos">
+                                <img src={nomevari1} alt=""></img>
+                            </div>
+                    </div>
+                </div>
+                <div className="card-produto">
+                        <div className="fundo-bg">
+                        <div className="rotulo">
+                            <h2>Novo drop </h2>
+                            <h2>Coleção</h2>
+                            <button><b className="button-comprar">Comprar</b></button></div>
+                            <b className="button-desconto">30% OFF</b>
+                            <div className="imagem-produtos">
+                                <img src={nomevari2} alt=""></img>
+                            </div>
+                    </div>
+                </div>
+                <div className="card-produto">
+                        <div className="fundo-bg">
+                        <div className="rotulo">
+                            <h2>Novo </h2>
+                            <h2>Beats Bass</h2>
+                            <button><b className="button-comprar">Comprar</b></button></div>
+                            <b className="button-desconto">30% OFF</b>
+                            <div className="imagem-produtos">
+                                <img src={nomevari3} alt=""></img>
+                            </div>
                     </div>
                 </div>
 
-                <div className="product-card">
-                    <img src={nomevari2} alt="" />
-                        <h2>Nova coleção Adidas</h2>
-                        <button>Comprar</button>
-
-                </div>
-
-                <div className="product-card">
-                    <img src= {nomevari3} alt="" />
-                        <h2>Novo Beats Bass</h2>
-                        <button>Comprar</button>
-
-                </div>
-            </section></div>
+            </section>
+        </div>
     )
 }
