3c2c68baa065284cd89553f47e538e9685080952
HERMES BARBOSA  PEREIRA <HERMES6313@DIGITALCOLLEGE.LOCAL>
2024-07-23 10:21:23 -0300

    Adicionado ProductCard

--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -2,40 +2,23 @@ import { useEffect } from "react";
 import { useState } from "react"
 import produtos from './ProductListingData.json'
 import './ProductListing.css'
+import ProductCard from "./ProductCard";
 
-function ProductListing () {
+function ProductListing() {
 
     let [listaExibicao, setListaExibicao] = useState([]);
 
-    const exibirProduto = ({desconto, imagem, tipo, nome, preco_normal, preco_descontado}) => {
-
-        return (
-            <>
-                <div className="div-products-item">
-                    <div className="div-products-item1">
-                        <p className="products-p-off">{desconto}</p>
-                        <div className="div-products-img-div">
-                            <img className="products-img" src={imagem}></img>
-                        </div>
-                    </div>
-                    <div className="div-products-item2">
-                        <p className="products-p1">{tipo}</p>
-                        <p className="products-p2">{nome}</p>
-                        <div className="products-price">
-                            <p className="products-p3">$ {preco_normal}</p><p className="products-p4">$ {preco_descontado}</p>
-                        </div>
-                    </div>
-                </div>
-            </>
-        )
-    }
-    
     useEffect(() => {
         exibirProdutos();
     }, [produtos]);
 
     const exibirProdutos = () => {
-        const novaListaExibicao = produtos.map(produto => exibirProduto(produto));
+        let novaListaExibicao = [];
+        for (let i = 0; i < produtos.length; i++) {
+            if (i < 8) {
+                novaListaExibicao.push(ProductCard(produtos[i]));
+            };
+        };
         setListaExibicao(novaListaExibicao);
     };
 
