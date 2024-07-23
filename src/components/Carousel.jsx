af604b0db204ce46c35cfd1897848bfd7eb651b1
Dâmarys Santos <damarysdev@gmail.com>
2024-07-23 13:36:07 +0000

    reiniciando o carrossel

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,22 @@
+import "./Carousel.css"
+
+export default function carrosel(){
+    return (
+        <div>
+            <div>
+                <p>Melhores ofertas personalizadas</p>
+                <h1>Queima de estoque Nike 🔥</h1>
+                <p>lorem ipsum</p>
+                <button>Ver Ofertas</button>
+            </div>
+            <div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
