bd48745ce66e3127980e06de869d07dc8299b913
Douglas <douglas.l2.hunter@gmail.com>
2024-07-24 08:31:24 -0300

    preciso organizar, precisei fazer uma consulta depois arrumo o resto

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import OfertaESpecial from './components/OfertaESpecial'
 import NovasColecoes from './components/NovasColecoes'
 import Rodape from './components/Rodape'
 import ProductListing from './components/ProductListing'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
+      <Header/>
       <NovasColecoes></NovasColecoes>
       <ProductListing tamanho={8}/>
       <OfertaESpecial />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
-import {logo} from "./src/assets/header-imgs/logo-digital.png"
-import { carrinho } from "./src/assets/header-imgs/carrinho-inteligente"
+import "./Header.css"
+import logo from "../assets/lupa.png"
+import lupa from "../assets/logo-digital.png"
+import carrinho  from "../assets/carrinho.png"
 
-export default function Header() {
+export default function Header () {
     return (
         <div className="header-conteiner">
             <div className="nav-header">
-                <a className="digital-store-header" href="#"><img className="logo-digital" src={logo} alt=""> Digital Store</a>
-                <input className="input-header" type="text" name="" id="" placeholder="Pesquisar produto...">
-                    <img className="search-icon" src="images/img-header/lupa.png" alt="">
+                <a className="digital-store-header" href="#"><img className="logo-digital" src={logo} alt="" /> Digital Store</a>
+                <input className="input-header" type="text" name="" id="" placeholder="Pesquisar produtos" />
+                    <img className="search-icon" src={lupa} alt="" />
                         <a className="cadastrar" href="">Cadastre-se</a>
-
                         <div className="navigation-header">
                             <ol className="ol-header">
                                 <a href="">
@@ -29,10 +30,10 @@ export default function Header() {
                         </div>
                         <div className="style-button">
                             <button className="entrar-header">Entrar</button>
-                            <img className="carrinho" src={carrinho} alt="">
+                            <img className="carrinho" src={carrinho} alt="carrinho-de-compras" />
                         </div>
                     </div>
-
+        
             </div>
             )
 }
